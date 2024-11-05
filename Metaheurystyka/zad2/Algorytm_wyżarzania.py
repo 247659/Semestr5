7919f08d313402add1f5628d019f19206283fa5d
CountToFour <247806@edu.p.lodz.pl>
2024-11-05 21:20:16 +0100

    metaheurystyki -> zad2 works

--- Metaheurystyka/zad2/Algorytm_wyżarzania.py
+++ Metaheurystyka/zad2/Algorytm_wyżarzania.py
@@ -13,62 +13,60 @@ def func2(x):
     return wynik
 
 def example1(t, a, epoki, proby):
-    t = t
-    a = a
-    epoki = epoki
-    proby = proby
-
     x1 = random.uniform(-15, 15)
     y1 = random.uniform(-15, 15)
-    f1 = func1(x1, y1)
+
+    best_result = func1(x1, y1)
+    best_x = x1
+    best_y = y1
 
     for i in range(epoki):
         for j in range(proby):
-
+            f1 = func1(x1, y1)
             x2 = random.uniform(max(-15.0, x1 - 0.04 * t), min(15.0, x1 + 0.04 * t))
             y2 = (random.uniform(max(-15.0, y1 - 0.04 * t), min(15.0, y1 + 0.04 * t)))
             f2 = func1(x2, y2)
 
 
-            if f1 < f2:
+            if f1 < f2 or (math.exp((f1 - f2) / t)) > random.uniform(0, 1):
                 x1 = x2
                 y1 = y2
-                f1 = f2
-            elif (math.exp((f1 - f2) / t)) > random.uniform(0, 1):
-                x1 = x2
-                y1 = y2
-                f1 = f2
+
+            if func1(x1, y1) > best_result:
+                best_x = x1
+                best_y = y1
+                best_result = func1(x1, y1)
+
         t *= a
 
-    return (x1, y1, f1)
+    return (best_x, best_y, best_result)
 
 def example2(t, a, epoki, proby):
-    t = t
-    a = a
-    epoki = epoki
-    proby = proby
 
     x1 = random.uniform(-1, 2)
     f1 = func2(x1)
+    best_x = x1
+    best_result = f1
 
     for i in range(epoki):
         for j in range(proby):
-
+            f1 = func2(x1)
             x2 = random.uniform(max(-1.0, x1 - 0.2 * t), min(2.0, x1 + 0.2 * t))
             f2 = func2(x2)
 
-            if f1 < f2:
+            if f1 < f2 or (math.exp((f1 - f2) / t)) > random.uniform(0, 1):
                 x1 = x2
-                f1 = f2
-            elif (math.exp((f1 - f2) / t)) > random.uniform(0, 1):
-                x1 = x2
-                f1 = f2
+
+            if func2(x1) > best_result:
+                best_x = x1
+                best_result = func2(x1)
+
         t *= a
 
-    return (x1, f1)
+    return (best_x, best_result)
 
 if __name__ == '__main__':
-    x, y, f1 = example1(90, 0.999, 200, 3)
+    x, y, f1 = example1(90, 0.999, 300, 5)
     print(f"Ekstemum znalezione w punkcie: ({x:.2f}, {y:.2f}) o wartości {f1}")
 
     x2, f1_2 = example2(5, 0.997, 1200, 3)
